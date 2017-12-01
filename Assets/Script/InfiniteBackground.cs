30cdfc00963e4d45f008ccba4011192fe8e63ade
Barbara <barbarapagano93@gmail.com>
2017-12-01 20:40:08 +0100

    Modifiche varie scene
    - Inserita grafica di Cai nelle scene Livello1, Livello2, Menu, Livelli
    - Creato il livello1
    - Creato il livello 2
    - Inserito sfondo infinito al termine del livello
    - Aggiustata velocità magneti
    - Inseriti suoni, immagini di tutorial e particle effect negli assets

---
+++ Assets/Script/InfiniteBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteBackground : MonoBehaviour {
+
+    public float BackgroundUnion;
+    //Point of restart movement
+    public Transform PosInfiniteBackground;
+
+    private Vector3 StartPos;
+
+    void Start()
+    {
+        StartPos = PosInfiniteBackground.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < BackgroundUnion)
+            transform.position = StartPos;
+	}
+}
