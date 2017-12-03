86ca2adc0bbcc7e9c00ae2929088ac76cfa3e9c0
Barbara <barbarapagano93@gmail.com>
2017-12-03 22:26:21 +0100

    Modifiche scene Levels e Menu
    - Inserimento immagini scena Levels con impostazione delle stelle e dei livelli bloccati.
    - Inserimento animazione scena Menu
    - Inserimento giusta immagine dei coins
    - Aggiunta suono al click

---
+++ Assets/Script/AnimationControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour {
+
+    public Animation[] MagnetsAnim;
+    private int NumAnim;
+    
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(AnimController(NumAnim, 3f));
+	}
+
+    IEnumerator AnimController(int Num, float time)
+    {
+        if (Num == 0)
+        {
+            MagnetsAnim[Num].Play();
+            MagnetsAnim[Num + 1].Stop();
+            yield return new WaitForSeconds(time);
+            NumAnim = 1;
+        }
+
+        if (Num == 1)
+        {
+            MagnetsAnim[Num].Play();
+            MagnetsAnim[Num - 1].Stop();
+            yield return new WaitForSeconds(time);
+            NumAnim = 0;
+        }
+    }
+}
---
+++ Assets/Script/SoundsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsController : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip Click;
+
+
+    public void ClickSoundPlay()
+    {
+        audioSource.PlayOneShot(Click);
+    }
+}
